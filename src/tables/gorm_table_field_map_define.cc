49bf42a069b91a9ee527e997cf6db560582d6dfd
hongchao <honghcao@ztgame.com>
2020-09-02 14:28:52 +0800

    handshake

--- src/tables/gorm_table_field_map_define.cc
+++ src/tables/gorm_table_field_map_define.cc
@@ -45,7 +45,7 @@ int GORM_InitTableSchemaInfo(PB_MSG_PTR pMsgPtr)
     GORM_PB_TABLE_SCHEMA_INFO_COLUMN *pColumn;
     pHandShake->mutable_header();
     pHandShake->set_version(1);
-    pHandShake->set_md5(1);
+    pHandShake->set_md5(0);
     // for table currency
     pInfo = pHandShake->add_schemas();
     if (pInfo == nullptr)
